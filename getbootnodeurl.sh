85ae9106673859372df68498cfcea14cdbecca5f
AndreyBulgakov <andbul0103@gmail.com>
2018-04-09 01:03:50 +0300

    Add name parameter for get bootnode enode.

--- getbootnodeurl.sh
+++ getbootnodeurl.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # reads current bootnode URL
-ENODE_LINE=$(docker logs ethereum-bootnode 2>&1 | grep enode | head -n 1)
+BOOT_NODE=$1
+CONTAINER_NAME="ethereum-$BOOT_NODE"
+ENODE_LINE=$(docker logs ${CONTAINER_NAME} 2>&1 | grep enode | head -n 1)
 # replaces localhost by container IP
 MYIP=$(docker exec ethereum-bootnode ifconfig eth0 | awk '/inet addr/{print substr($2,6)}')
 ENODE_LINE=$(echo $ENODE_LINE | sed "s/127\.0\.0\.1/$MYIP/g" | sed "s/\[\:\:\]/$MYIP/g")
